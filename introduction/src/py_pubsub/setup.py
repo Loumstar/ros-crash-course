b43105015a4ce718a47444560e2a8df03a6dc056
Louis Manestar <louis.manestar@gmail.com>
2022-05-19 19:48:51 +0100

    Completed a simple pub-sub ROS package.

---
+++ introduction/src/py_pubsub/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'py_pubsub'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='loumstar',
+    maintainer_email='loumstar@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = py_pubsub.py_pub:main',
+            'listener = py_pubsub.py_sub:main'
+        ],
+    },
+)
